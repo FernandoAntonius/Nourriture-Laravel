fddeb35d3a54b23cac5ba57fcbdcae0f9c385495
FernandoAntonius <fernandoanthonius_2428250014@mhs.mdp.ac.id>
2025-12-18 14:21:23 +0700

    route

--- routes/api.php
+++ routes/api.php
@@ -10,12 +10,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// API Routes untuk Food
 Route::apiResource('foods', FoodController::class);
-
-// API Routes untuk Recipe
 Route::apiResource('recipes', RecipeController::class);
 
-// API Routes untuk Food-Recipe (menambah/menghapus food dari recipe)
 Route::post('/food-recipes', [FoodRecipeController::class, 'store']);
 Route::delete('/food-recipes', [FoodRecipeController::class, 'destroy']);
